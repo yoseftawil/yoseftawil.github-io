c2cfccd5f581832aa0f56ddc08eb0afb78b04a10
yoseftawil <yoseftawil@gmail.com>
2020-08-21 01:41:10 +0300

    housekeeping and add new info, scss

--- assets/scripts/badge.js
+++ assets/scripts/badge.js
@@ -2,5 +2,16 @@ const badge = document.getElementById('thm_badge');
 const navbar = document.getElementById('navbar-top');
 
 navbar.appendChild(badge);
+fadeIn(badge)
 
-
+function fadeIn(element) {
+    let op = 0.1;  
+    let timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
---
+++ pages/about.md
@@ -0,0 +1,10 @@
+---
+layout: default
+permalink: /about/
+---
+
+# About Me  
+
+#### Need to make myself look good here
+
+[Resume](/resume/)
